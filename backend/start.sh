386b2970d743b5634743f6c7d139fdbae331e604
Shili Abdellatif <abdellatifshili226@gmail.com>
2026-07-23 18:07:31 +0100

    fix: move Apache port config to runtime start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -3,4 +3,9 @@ php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 php artisan migrate --force
+
+sed -i "s/Listen 80/Listen ${PORT:-10000}/" /etc/apache2/ports.conf
+sed -i "s/*:80/*:${PORT:-10000}/" /etc/apache2/sites-available/000-default.conf
+sed -i "s/:80/:${PORT:-10000}/" /etc/apache2/apache2.conf
+
 apache2-foreground
